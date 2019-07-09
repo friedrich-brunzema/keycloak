d430d3a0df46c17c953fc5ee5a653985c66921f5
Friedrich Brunzema <Friedrich.Brunzema@sciex.com>
2019-07-09 14:21:05 -0400

    Worksgit stgit st

--- csharp/OpenIdConnectLogin/Program.cs
+++ csharp/OpenIdConnectLogin/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web;
+using System.Xml;
 using HtmlAgilityPack;
 using RestSharp;
 
@@ -23,17 +26,58 @@ namespace OpenIdConnectLogin
     class Application
     {
         private RestClient _restClient;
+        private string _authSessionIdCookieValue;
+        private string _kcRestartCookieValue;
+        private string _sessionState;
+        private string _authorizationCode;
+        private string _redirectUri;
+
         public async Task<string> Run()
         {
             _restClient = new RestClient("http://localhost/auth/") {CookieContainer = new CookieContainer()};
-            var proxy = new WebProxy("127.0.0.1", 8866);
-            proxy.BypassProxyOnLocal = false;
-            proxy.UseDefaultCredentials = false;
-            //_restClient.Proxy = proxy;
+           
             var html = InitialAuthRequest();
             var response = await PostLoginRequest(html, "demo", "Xaris,,404");
+			// do a get on _redirectURI
+            var token = await GetTokens();
             return "";
         }
+
+        private async Task<string> GetTokens()
+        {
+			var request = new RestRequest("realms/oneomics/protocol/openid-connect/token", Method.POST);
+			var httpClientHandler = new HttpClientHandler { AllowAutoRedirect = false };
+			var client = new HttpClient(httpClientHandler);
+			client.DefaultRequestHeaders.Clear();
+			
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
+			client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+			client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
+			client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("en-US"));
+
+			var nvc = new List<KeyValuePair<string, string>>
+			{
+				new KeyValuePair<string, string>("grant_type", "authorization_code"),
+				new KeyValuePair<string, string>("code", $"{_authorizationCode}"),
+				new KeyValuePair<string, string>("redirect_uri", "http://localhost"),
+				new KeyValuePair<string, string>("client_id", $"api"),
+				new KeyValuePair<string, string>("client_secret", "cba440fe-f15b-41c2-b5c9-a8d306556172")
+			};
+			var rq = new HttpRequestMessage(HttpMethod.Post, new Uri("http://localhost/auth/realms/oneomics/protocol/openid-connect/token"));
+			rq.Content = new FormUrlEncodedContent(nvc);
+			rq.Headers.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Postman/7.2.2 Chrome/59.0.3071.115 Electron/1.8.8 Safari/537.36");
+			rq.Headers.Add("Connection", "keep-alive");
+			rq.Headers.Add("Cache-control", "no-cache");
+			rq.Headers.Add("Cookie", $"AUTH_SESSION_ID={_authSessionIdCookieValue}; KC_RESTART={_kcRestartCookieValue}");
+			using (var result = await client.SendAsync(rq))
+			{
+				using (var content = result.Content)
+				{
+					var res = await content.ReadAsStringAsync();
+					return res;
+				}
+			}
+		}
 
         private string InitialAuthRequest()
         {
@@ -49,55 +93,54 @@ namespace OpenIdConnectLogin
             request.Parameters.Add(new Parameter("","",ParameterType.GetOrPost));
 
             var response = _restClient.Get(request);
-            return response.IsSuccessful ? response.Content : string.Empty;
+            Debug.Assert(response.Cookies != null, "response.Cookies != null");
+            _authSessionIdCookieValue = response.Cookies.FirstOrDefault(x => x.Name == "AUTH_SESSION_ID")?.Value;
+            _kcRestartCookieValue = response.Cookies.FirstOrDefault(x => x.Name == "KC_RESTART")?.Value;
+
+			return response.IsSuccessful ? response.Content : string.Empty;
         }
 
         private async Task<string> PostLoginRequest(string html, string username, string password)
         {
-            var httpClientHandler = new HttpClientHandler
-            {
-                Proxy = new WebProxy("http://localhost:8866", false),
-                UseProxy = true
-            };
-            var request = new RestRequest("realms/oneomics/login-actions/authenticate", Method.POST);
-            var client = new HttpClient(httpClientHandler);
-            client.DefaultRequestHeaders.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
-            var url = AddRedirectParametersToRequestFromHtml(html, request.Parameters);
-            var nvc = new List<KeyValuePair<string, string>>();
-            nvc.Add(new KeyValuePair<string, string>("username",$"{username}"));
-            nvc.Add(new KeyValuePair<string, string>("password", $"{password}"));
-            var rq = new HttpRequestMessage(HttpMethod.Post, url);
-            rq.Content = new FormUrlEncodedContent(nvc);
-            rq.Headers.Add("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Postman/7.2.2 Chrome/59.0.3071.115 Electron/1.8.8 Safari/537.36");
-            rq.Headers.Add("Referer","http://localhost/auth/realms/oneomics/protocol/openid-connect/auth?response_type=code&state=12345&client_id=api&scope=openid&redirect_uri=http%3A%2F%2Flocalhost");
-            using (var result = await client.SendAsync(rq))
-            {
-                var content = await result.Content.ReadAsStringAsync();
-                return content;
-            }
-           
-            
-            
-            
-            //request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-            //request.AddHeader("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Postman/7.2.2 Chrome/59.0.3071.115 Electron/1.8.8 Safari/537.36");
-            //request.AddHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-            //request.AddHeader("Accept-Encoding", "gzip, deflate");
-            //request.AddHeader("Referer", "http://localhost/auth/realms/oneomics/protocol/openid-connect/auth?response_type=code&state=12345&client_id=api&scope=openid&redirect_uri=http%3A%2F%2Flocalhost");
-            
-            
-            //AddRedirectParametersToRequestFromHtml(html, request.Parameters);
-            //request.AddParameter("Referer", "http://localhost/auth/realms/oneomics/protocol/openid-connect/auth?response_type=code&state=12345&client_id=api&scope=openid&redirect_uri=http%3A%2F%2Flocalhost",
-            //    ParameterType.HttpHeader);
-            //request.Parameters.Add(new Parameter("redirect_uri","http://localhost",ParameterType.GetOrPost));
-            //request.AddParameter("username", $"{username}", ParameterType.RequestBody);
-            //request.AddParameter("password", $"{password}", ParameterType.RequestBody);
-            //request.RequestFormat = DataFormat.None;
-            
-            //var response = _restClient.Execute(request);
-            //return response.IsSuccessful ? response.Content : string.Empty;
-        }
+			var request = new RestRequest("realms/oneomics/login-actions/authenticate", Method.POST);
+			var httpClientHandler = new HttpClientHandler {AllowAutoRedirect = false};
+
+			var client = new HttpClient(httpClientHandler);
+			client.DefaultRequestHeaders.Clear();
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xhtml+xml"));
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml",0.9));
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("image/webp"));
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("image/apng"));
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*", 0.8));
+			client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+			client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
+			client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("br"));
+			client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("en-US"));
+			
+			var url = AddRedirectParametersToRequestFromHtml(html, request.Parameters);
+			var nvc = new List<KeyValuePair<string, string>>
+			{
+				new KeyValuePair<string, string>("username", $"{username}"),
+				new KeyValuePair<string, string>("password", $"{password}")
+			};
+			var rq = new HttpRequestMessage(HttpMethod.Post, url);
+			rq.Headers.Add("Cookie", $"AUTH_SESSION_ID={_authSessionIdCookieValue}; KC_RESTART={_kcRestartCookieValue}");
+			rq.Content = new FormUrlEncodedContent(nvc);
+			rq.Headers.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Postman/7.2.2 Chrome/59.0.3071.115 Electron/1.8.8 Safari/537.36");
+			
+			rq.Headers.Add("Referer", "http://localhost/auth/realms/oneomics/protocol/openid-connect/auth?response_type=code&state=12345&client_id=api&scope=openid&redirect_uri=http%3A%2F%2Flocalhost");
+			using (var result = await client.SendAsync(rq))
+			{
+				// this results in a 301 redirect, but with the secrets in the redirect URI location.
+				var hdr = result.Headers.FirstOrDefault(x => x.Key == "Location");
+				_redirectUri = hdr.Value.FirstOrDefault();
+				var uri = new Uri(_redirectUri);
+				_sessionState = HttpUtility.ParseQueryString(uri.Query).Get("session_state");
+				_authorizationCode = HttpUtility.ParseQueryString(uri.Query).Get("code");
+				return "";
+			}
+		}
         
         private string AddRedirectParametersToRequestFromHtml(string html, List<Parameter> parameters)
         {
