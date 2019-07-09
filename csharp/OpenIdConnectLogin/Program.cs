eda8ef10814ad867100158422577b7c1cdf944e0
Friedrich Brunzema <Friedrich.Brunzema@sciex.com>
2019-07-09 15:19:09 -0400

    Some refactoring

--- csharp/OpenIdConnectLogin/Program.cs
+++ csharp/OpenIdConnectLogin/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright SCIEX 2019. All rights reserved.
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -7,117 +9,130 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web;
-using System.Xml;
 using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 
 namespace OpenIdConnectLogin
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            Console.WriteLine("OpenIdConnectSpike");
-            var app = new Application();
-            await app.Run();
-        }
-    }
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var app = new Application();
+			var result = await app.Run();
+			Console.WriteLine(Environment.NewLine + result);
+		}
+	}
 
-    class Application
-    {
-        private RestClient _restClient;
-        private string _authSessionIdCookieValue;
-        private string _kcRestartCookieValue;
-        private string _sessionState;
-        private string _authorizationCode;
-        private string _redirectUri;
+	class Application
+	{
+		private const string KeyCloakUrl = "http://localhost/auth/";
 
-        public async Task<string> Run()
-        {
-            _restClient = new RestClient("http://localhost/auth/") {CookieContainer = new CookieContainer()};
-           
-            var html = InitialAuthRequest();
-            var response = await PostLoginRequest(html, "demo", "Xaris,,404");
-			// do a get on _redirectURI
-            var token = await GetTokens();
-            return "";
-        }
+		private const string TokenEndPointSubUrl =
+			"realms/oneomics/protocol/openid-connect/token";
+		private const string AuthEndPointSubUrl =
+			"realms/oneomics/protocol/openid-connect/auth";
+		private const string AuthenticateEndPointSubUrl =
+			"realms/oneomics/login-actions/authenticate";
+		private const string RedirectUrl = "http://localhost";
+		private const string ClientSecret = "cba440fe-f15b-41c2-b5c9-a8d306556172";
+		private const string ClientId = "api";
+		private const string UserAgent =
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko)"
+			+ " Postman/7.2.2 Chrome/59.0.3071.115 Electron/1.8.8 Safari/537.36";
 
-        private async Task<string> GetTokens()
-        {
-			var request = new RestRequest("realms/oneomics/protocol/openid-connect/token", Method.POST);
-			var httpClientHandler = new HttpClientHandler { AllowAutoRedirect = false };
-			var client = new HttpClient(httpClientHandler);
-			client.DefaultRequestHeaders.Clear();
-			
-			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
-			client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
-			client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
-			client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("en-US"));
+		private string _authorizationCode;
+		private string _authSessionIdCookieValue;
+		private string _kcRestartCookieValue;
+		private string _redirectUri;
+		private RestClient _restClient;
+		private string _username;
+		private string _password;
 
-			var nvc = new List<KeyValuePair<string, string>>
+		public async Task<string> Run()
+		{
+			Console.WriteLine("OpenIdConnect JWT-Token getter hack, v1.0  (c) SCIEX 2019" + Environment.NewLine);
+			Console.WriteLine($"Signing into; {KeyCloakUrl}");
+			_restClient = new RestClient(KeyCloakUrl)
+				{CookieContainer = new CookieContainer()};
+			Console.Write("Enter your username: ");
+			_username = Console.ReadLine();
+			Console.Write("Enter your password: ");
+			_password = PasswordHelper.GetPassword();
+
+			var formattedTokensAsJson = string.Empty;
+			try
 			{
-				new KeyValuePair<string, string>("grant_type", "authorization_code"),
-				new KeyValuePair<string, string>("code", $"{_authorizationCode}"),
-				new KeyValuePair<string, string>("redirect_uri", "http://localhost"),
-				new KeyValuePair<string, string>("client_id", $"api"),
-				new KeyValuePair<string, string>("client_secret", "cba440fe-f15b-41c2-b5c9-a8d306556172")
-			};
-			var rq = new HttpRequestMessage(HttpMethod.Post, new Uri("http://localhost/auth/realms/oneomics/protocol/openid-connect/token"));
-			rq.Content = new FormUrlEncodedContent(nvc);
-			rq.Headers.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Postman/7.2.2 Chrome/59.0.3071.115 Electron/1.8.8 Safari/537.36");
-			rq.Headers.Add("Connection", "keep-alive");
-			rq.Headers.Add("Cache-control", "no-cache");
-			rq.Headers.Add("Cookie", $"AUTH_SESSION_ID={_authSessionIdCookieValue}; KC_RESTART={_kcRestartCookieValue}");
-			using (var result = await client.SendAsync(rq))
-			{
-				using (var content = result.Content)
-				{
-					var res = await content.ReadAsStringAsync();
-					return res;
-				}
+				var html = InitialAuthRequest();
+				await PostLoginRequest(html, _username, _password);
+				var tokensJson = await GetTokensFromTokenEndpoint();
+				formattedTokensAsJson = JToken.Parse(tokensJson).ToString(Formatting.Indented);
+				Console.WriteLine(formattedTokensAsJson);
 			}
+			catch
+			{
+				Console.WriteLine("Could not log you in, sorry!");
+			}
+			return formattedTokensAsJson;
 		}
 
-        private string InitialAuthRequest()
-        {
-            var request = new RestRequest("realms/oneomics/protocol/openid-connect/auth", Method.GET);
-            request.Parameters.Add(new Parameter("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Postman/7.2.2 Chrome/59.0.3071.115 Electron/1.8.8 Safari/537.36",ParameterType.HttpHeader));
-            request.Parameters.Add(new Parameter("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",ParameterType.HttpHeader));
-            
-            //request.Parameters.Add(new Parameter("state","",ParameterType.GetOrPost));
-            request.Parameters.Add(new Parameter("client_id","api",ParameterType.GetOrPost));
-            request.Parameters.Add(new Parameter("scope","openid",ParameterType.GetOrPost));
-            request.Parameters.Add(new Parameter("response_type","code",ParameterType.GetOrPost));
-            request.Parameters.Add(new Parameter("redirect_uri","http://localhost",ParameterType.GetOrPost));
-            request.Parameters.Add(new Parameter("","",ParameterType.GetOrPost));
+		private string InitialAuthRequest()
+		{
+			var request = new RestRequest(AuthEndPointSubUrl, Method.GET);
+			request.Parameters.Add(new Parameter("User-Agent", UserAgent, ParameterType.HttpHeader));
+			request.Parameters.Add(new Parameter("Accept",
+				"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+				ParameterType.HttpHeader));
+			request.Parameters.Add(new Parameter("client_id", "api",
+				ParameterType.GetOrPost));
+			request.Parameters.Add(new Parameter("scope", "openid",
+				ParameterType.GetOrPost));
+			request.Parameters.Add(new Parameter("response_type", "code",
+				ParameterType.GetOrPost));
+			request.Parameters.Add(new Parameter("redirect_uri", RedirectUrl,
+				ParameterType.GetOrPost));
 
-            var response = _restClient.Get(request);
-            Debug.Assert(response.Cookies != null, "response.Cookies != null");
-            _authSessionIdCookieValue = response.Cookies.FirstOrDefault(x => x.Name == "AUTH_SESSION_ID")?.Value;
-            _kcRestartCookieValue = response.Cookies.FirstOrDefault(x => x.Name == "KC_RESTART")?.Value;
+			var response = _restClient.Get(request);
+			Debug.Assert(response.Cookies != null, "response.Cookies != null");
+			_authSessionIdCookieValue = response.Cookies
+				.FirstOrDefault(x => x.Name == "AUTH_SESSION_ID")?.Value;
+			_kcRestartCookieValue = response.Cookies
+				.FirstOrDefault(x => x.Name == "KC_RESTART")?.Value;
 
 			return response.IsSuccessful ? response.Content : string.Empty;
-        }
+		}
 
-        private async Task<string> PostLoginRequest(string html, string username, string password)
-        {
-			var request = new RestRequest("realms/oneomics/login-actions/authenticate", Method.POST);
-			var httpClientHandler = new HttpClientHandler {AllowAutoRedirect = false};
+		private async Task PostLoginRequest(string html, string username,
+		string password)
+		{
+			var request = new RestRequest(AuthenticateEndPointSubUrl, Method.POST);
+			var httpClientHandler = new HttpClientHandler { AllowAutoRedirect = false };
 
 			var client = new HttpClient(httpClientHandler);
 			client.DefaultRequestHeaders.Clear();
-			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
-			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xhtml+xml"));
-			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml",0.9));
-			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("image/webp"));
-			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("image/apng"));
-			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*", 0.8));
-			client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
-			client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
-			client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("br"));
-			client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("en-US"));
-			
+			client.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("text/html"));
+			client.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("application/xhtml+xml"));
+			client.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("application/xml", 0.9));
+			client.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("image/webp"));
+			client.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("image/apng"));
+			client.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("*/*", 0.8));
+			client.DefaultRequestHeaders.AcceptEncoding.Add(
+				new StringWithQualityHeaderValue("gzip"));
+			client.DefaultRequestHeaders.AcceptEncoding.Add(
+				new StringWithQualityHeaderValue("deflate"));
+			client.DefaultRequestHeaders.AcceptEncoding.Add(
+				new StringWithQualityHeaderValue("br"));
+			client.DefaultRequestHeaders.AcceptLanguage.Add(
+				new StringWithQualityHeaderValue("en-US"));
+
 			var url = AddRedirectParametersToRequestFromHtml(html, request.Parameters);
 			var nvc = new List<KeyValuePair<string, string>>
 			{
@@ -127,47 +142,87 @@ namespace OpenIdConnectLogin
 			var rq = new HttpRequestMessage(HttpMethod.Post, url);
 			rq.Headers.Add("Cookie", $"AUTH_SESSION_ID={_authSessionIdCookieValue}; KC_RESTART={_kcRestartCookieValue}");
 			rq.Content = new FormUrlEncodedContent(nvc);
-			rq.Headers.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Postman/7.2.2 Chrome/59.0.3071.115 Electron/1.8.8 Safari/537.36");
-			
-			rq.Headers.Add("Referer", "http://localhost/auth/realms/oneomics/protocol/openid-connect/auth?response_type=code&state=12345&client_id=api&scope=openid&redirect_uri=http%3A%2F%2Flocalhost");
+			rq.Headers.Add("User-Agent", UserAgent);
+			rq.Headers.Add("prompt", "login"); // force re-login -- openid spec section 3.1.2.3
+
 			using (var result = await client.SendAsync(rq))
 			{
 				// this results in a 301 redirect, but with the secrets in the redirect URI location.
 				var hdr = result.Headers.FirstOrDefault(x => x.Key == "Location");
 				_redirectUri = hdr.Value.FirstOrDefault();
 				var uri = new Uri(_redirectUri);
-				_sessionState = HttpUtility.ParseQueryString(uri.Query).Get("session_state");
 				_authorizationCode = HttpUtility.ParseQueryString(uri.Query).Get("code");
-				return "";
 			}
 		}
-        
-        private string AddRedirectParametersToRequestFromHtml(string html, List<Parameter> parameters)
-        {
-            var doc = new HtmlDocument();
-            doc.LoadHtml(html);
 
-            var nd = doc.DocumentNode.SelectSingleNode("//form[@id='kc-form-login']");
-            var actionUri = nd.Attributes["action"].Value;
-            var fixedUri = actionUri.Replace("&amp;", "&");
-            var uri = new Uri(fixedUri);
-            parameters.Add(
-                new Parameter("session_code", 
-                    HttpUtility.ParseQueryString(uri.Query).Get("session_code"),
-                    ParameterType.UrlSegment));
-            parameters.Add(
-                new Parameter("execution", 
-                    HttpUtility.ParseQueryString(uri.Query).Get("execution"),
-                    ParameterType.UrlSegment));
-            parameters.Add(
-                new Parameter("client_id", 
-                    HttpUtility.ParseQueryString(uri.Query).Get("client_id"),
-                    ParameterType.UrlSegment));
-            parameters.Add(
-                new Parameter("tab_id", 
-                    HttpUtility.ParseQueryString(uri.Query).Get("tab_id"),
-                    ParameterType.UrlSegment));
-            return fixedUri;
-        }
-    }
+		private async Task<string> GetTokensFromTokenEndpoint()
+		{
+			var request = new RestRequest(TokenEndPointSubUrl, Method.POST);
+			var httpClientHandler = new HttpClientHandler {AllowAutoRedirect = false};
+			var client = new HttpClient(httpClientHandler);
+			client.DefaultRequestHeaders.Clear();
+
+			client.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("*/*"));
+			client.DefaultRequestHeaders.AcceptEncoding.Add(
+				new StringWithQualityHeaderValue("gzip"));
+			client.DefaultRequestHeaders.AcceptEncoding.Add(
+				new StringWithQualityHeaderValue("deflate"));
+			client.DefaultRequestHeaders.AcceptLanguage.Add(
+				new StringWithQualityHeaderValue("en-US"));
+
+			var nvc = new List<KeyValuePair<string, string>>
+			{
+				new KeyValuePair<string, string>("grant_type", "authorization_code"),
+				new KeyValuePair<string, string>("code", $"{_authorizationCode}"),
+				new KeyValuePair<string, string>("redirect_uri", RedirectUrl),
+				new KeyValuePair<string, string>("client_id", ClientId),
+				new KeyValuePair<string, string>("client_secret", ClientSecret)
+			};
+			const string tokenUrl = KeyCloakUrl + TokenEndPointSubUrl;
+			var rq = new HttpRequestMessage(HttpMethod.Post, new Uri(tokenUrl));
+			rq.Content = new FormUrlEncodedContent(nvc);
+			rq.Headers.Add("User-Agent", UserAgent);
+			rq.Headers.Add("Connection", "keep-alive");
+			rq.Headers.Add("Cache-control", "no-cache");
+			rq.Headers.Add("Cookie",
+				$"AUTH_SESSION_ID={_authSessionIdCookieValue}; KC_RESTART={_kcRestartCookieValue}");
+			using (var result = await client.SendAsync(rq))
+			{
+				using (var content = result.Content)
+				{
+					var res = await content.ReadAsStringAsync();
+					return res;
+				}
+			}
+		}
+	
+		private string AddRedirectParametersToRequestFromHtml(string html,
+			List<Parameter> parameters)
+		{
+			var doc = new HtmlDocument();
+			doc.LoadHtml(html);
+			var nd = doc.DocumentNode.SelectSingleNode("//form[@id='kc-form-login']");
+			var actionUri = nd.Attributes["action"].Value;
+			var fixedUri = actionUri.Replace("&amp;", "&");
+			var uri = new Uri(fixedUri);
+			parameters.Add(
+				new Parameter("session_code",
+					HttpUtility.ParseQueryString(uri.Query).Get("session_code"),
+					ParameterType.UrlSegment));
+			parameters.Add(
+				new Parameter("execution",
+					HttpUtility.ParseQueryString(uri.Query).Get("execution"),
+					ParameterType.UrlSegment));
+			parameters.Add(
+				new Parameter("client_id",
+					HttpUtility.ParseQueryString(uri.Query).Get("client_id"),
+					ParameterType.UrlSegment));
+			parameters.Add(
+				new Parameter("tab_id",
+					HttpUtility.ParseQueryString(uri.Query).Get("tab_id"),
+					ParameterType.UrlSegment));
+			return fixedUri;
+		}
+	}
 }
